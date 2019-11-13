105d61101fa9736d406c1ec0675c84602ed5e164
alaamahasneh <alaa.ibraheem97@yahoo.com>
2019-11-13 18:01:59 +0200

    NEW

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 in lab03:
 we creat a guessing game ( its funny game ^_^)
 we creat many question solved by user , we use to creat this questions many of if satment,while loop , for loop and array
--- js/app.js
+++ js/app.js
@@ -1,143 +1,143 @@
 'use strict'
-// var userName = prompt(' What is your name?');
+var userName = prompt(' What is your name?');
 
-// while (parseInt(userName)) {
-//     userName = prompt(' your name is so necessary , please input your name ');
-// }
-// alert('welcome' + ' ' + userName);
+while (parseInt(userName)) {
+    userName = prompt(' your name is so necessary , please input your name ');
+}
+alert('welcome' + ' ' + userName);
 
 var counter = 1;
-// alert('Now let us start play guss game ^_^ , please answer with yes or no')
-// //(Question 1)//
+alert('Now let us start play guss game ^_^ , please answer with yes or no')
+//(Question 1)//
 
-// var q1 = prompt('Do you think that i love cats?').toLocaleUpperCase();
+var q1 = prompt('Do you think that i love cats?').toLocaleUpperCase();
 
-// while (q1 !== 'yes'&& q1 !=='y' && q1 !== 'no'&& q1 !=='n') {
-//     q1 = prompt('sorry but this is Yes or No question?')
-// }
+while (q1 !== 'yes'&& q1 !=='y' && q1 !== 'no'&& q1 !=='n') {
+    q1 = prompt('sorry but this is Yes or No question?')
+}
 
-// switch (q1) {
-//     case 'yes':
-//     case 'y':
-//         alert('Correct, i love cats so much');
-//         alert(counter++);
-//                 break;
-//     case 'no':
-//     case 'n':
-//         alert('wrong , i love cats');
-//         break;
-//     default:
-//         alert('Try again');
-//         break;
-// }
-// //(Question 2)//
-// var q2 = prompt('Do you think my favourite dish is mansaf?').toLocaleUpperCase();
+switch (q1) {
+    case 'yes':
+    case 'y':
+        alert('Correct, i love cats so much');
+        alert(counter++);
+                break;
+    case 'no':
+    case 'n':
+        alert('wrong , i love cats');
+        break;
+    default:
+        alert('Try again');
+        break;
+}
+//(Question 2)//
+var q2 = prompt('Do you think my favourite dish is mansaf?').toLocaleUpperCase();
 
-// while (q2 !== 'yes'&& q2 !=='y' && q2 !== 'no'&& q2 !=='n') {
-//     q2 = prompt("sorry but this is Yes or No question?")
-// }
+while (q2 !== 'yes'&& q2 !=='y' && q2 !== 'no'&& q2 !=='n') {
+    q2 = prompt("sorry but this is Yes or No question?")
+}
 
-// switch (q2) {
-//     case 'yes':
-//     case 'y':
-//         alert('i dont like mansaf');
-//         alert(counter++);
-//                 break;
-//     case 'no':
-//     case 'n':
-//         alert('i prefer pizza');
-//         break;
-//     default:
-//         alert('Try again');
-//         break;
-// }
-// //(Question 3)//
-// var q3 = prompt('Do you think im from jordan?"').toLocaleUpperCase();
+switch (q2) {
+    case 'yes':
+    case 'y':
+        alert('i dont like mansaf');
+        alert(counter++);
+                break;
+    case 'no':
+    case 'n':
+        alert('i prefer pizza');
+        break;
+    default:
+        alert('Try again');
+        break;
+}
+//(Question 3)//
+var q3 = prompt('Do you think im from jordan?"').toLocaleUpperCase();
 
-// while (q3 !== 'yes'&& q3 !=='y' && q3 !== 'no'&& q3 !=='n') {
-//     q3 = prompt("sorry but this is Yes or No question?")
+while (q3 !== 'yes'&& q3 !=='y' && q3 !== 'no'&& q3 !=='n') {
+    q3 = prompt("sorry but this is Yes or No question?")
 
-// }
+}
 
-// switch (q3) {
-//     case 'yes':
-//     case 'y':
-//         alert('yah im ');
-//         alert(counter++);
-//                 break;
-//     case 'no':
-//     case 'n':
-//         alert('wrong im jordanin');
-//         break;
-//     default:
-//         alert('Try again');
-//         break;
-// }
+switch (q3) {
+    case 'yes':
+    case 'y':
+        alert('yah im ');
+        alert(counter++);
+                break;
+    case 'no':
+    case 'n':
+        alert('wrong im jordanin');
+        break;
+    default:
+        alert('Try again');
+        break;
+}
 
-// //(Question 4)//
-// var q4 = prompt('Do you think that i love programming language?').toLocaleUpperCase();
+//(Question 4)//
+var q4 = prompt('Do you think that i love programming language?').toLocaleUpperCase();
 
-// while (q4 !== 'yes'&& q4 !=='y' && q4 !== 'no'&& q4 !=='n') {
-//     q4 = prompt("sorry but this is Yes or No question?")
+while (q4 !== 'yes'&& q4 !=='y' && q4 !== 'no'&& q4 !=='n') {
+    q4 = prompt("sorry but this is Yes or No question?")
 
-// }
+}
 
-// switch (q4) {
-//     case 'yes':
-//     case 'y':
-//         alert('yah i love it ');
-//         alert(counter++);
-//                 break;
-//     case 'no':
-//     case 'n':
-//         alert('wrong i love it');
-//         break;
-//     default:
-//         alert('Try again');
-//         break;
-// }
-// //(Question 5)//
-// var q5 = prompt('do you think that im an civil engineer?').toLocaleUpperCase();
+switch (q4) {
+    case 'yes':
+    case 'y':
+        alert('yah i love it ');
+        alert(counter++);
+                break;
+    case 'no':
+    case 'n':
+        alert('wrong i love it');
+        break;
+    default:
+        alert('Try again');
+        break;
+}
+//(Question 5)//
+var q5 = prompt('do you think that im an civil engineer?').toLocaleUpperCase();
 
-// while (q5!== 'yes'&& q5 !=='y' && q5 !== 'no'&& q5 !=='n') {
-//     q5 = prompt("sorry but this is Yes or No question?")
+while (q5!== 'yes'&& q5 !=='y' && q5 !== 'no'&& q5 !=='n') {
+    q5 = prompt("sorry but this is Yes or No question?")
 
-// }
+}
 
-// switch (q5) {
-//     case 'yes':
-//     case 'y':
-//         alert('yah im ');
-//         alert(counter++);
-//                 break;
-//     case 'no':
-//     case 'n':
-//         alert('wrong im civil engineer');
-//         break;
-//     default:
-//         alert('Try again');
-//         break;
-// }
+switch (q5) {
+    case 'yes':
+    case 'y':
+        alert('yah im ');
+        alert(counter++);
+                break;
+    case 'no':
+    case 'n':
+        alert('wrong im civil engineer');
+        break;
+    default:
+        alert('Try again');
+        break;
+}
 
-// alert("Your score is: " + counter );
-// // (question 6)//
+alert("Your score is: " + counter );
+// (question 6)//
 
-// for (var trail = 0; trail < 4; trail++) {
-//     var old = prompt("Guess how old i am ! Note that : You have 4 trials")
-//     if (old == 22) {
-//         alert("Yes exactly! im 22 years old");
-//         // console.log(old);
-//         break;
-//     } else if (old > 22) {
-//         alert("too high");
-//         // console.log(old);
-//     } else if (old < 22) {
-//         alert(" too low");
-//         // console.log(old);
-//     }
-// }
-// alert("I am 22 years old");
-// alert("Your score is: " + counter);
+for (var trail = 0; trail < 4; trail++) {
+    var old = prompt("Guess how old i am ! Note that : You have 4 trials")
+    if (old == 22) {
+        alert("Yes exactly! im 22 years old");
+        // console.log(old);
+        break;
+    } else if (old > 22) {
+        alert("too high");
+        // console.log(old);
+    } else if (old < 22) {
+        alert(" too low");
+        // console.log(old);
+    }
+}
+alert("I am 22 years old");
+alert("Your score is: " + counter);
 
 
 //(Question 7)//
